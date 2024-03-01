2963fb129db1235ad714ad5fc71cda948055842a
Romain <145614911+RomainJac@users.noreply.github.com>
2024-03-01 14:43:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-maven-quickstart-latest
+
+ this is an up-to-date base archetype for quick starting modern java CLI apps 
---
+++ src/main/java/fr/pantheonsorbonne/cri/App.java
@@ -0,0 +1,18 @@
+package fr.pantheonsorbonne.cri;
+
+/**
+ * The main class for the application.
+ */
+public final class App {
+
+    private App() {
+
+    };
+    /**
+     * main entrypoint for my class.
+     * @param args a bunch of string from the cli
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
