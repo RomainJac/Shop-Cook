7e699480234e3cb189f0b674d282232c9340602b
Lucian Lisnic <lisniclucian@gmail.com>
2024-03-01 16:26:01 +0100

    spring start

--- src/main/java/com/example/demo/ingredients/DietaryRestriction.java
+++ src/main/java/com/example/demo/ingredients/DietaryRestriction.java
@@ -2,4 +2,7 @@ package com.example.demo.ingredients;
 
 public class DietaryRestriction {
     private String name;
+    public DietaryRestriction(String name) {
+        this.name = name;
+    }
 }
