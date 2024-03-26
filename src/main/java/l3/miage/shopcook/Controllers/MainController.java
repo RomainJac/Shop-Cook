3cd1168b86c5253ddf952ffdd6b72419b3a38651
Romain <romainjacob012@gmail.com>
2024-03-26 10:12:56 +0100

    Little fix

--- src/main/java/l3/miage/shopcook/Controllers/MainController.java
+++ src/main/java/l3/miage/shopcook/Controllers/MainController.java
@@ -1,4 +1,4 @@
-package l3.miage.shopcook.controllers;
+package l3.miage.shopcook.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/l3/miage/shopcook/repositories/IngredientRepository.java
+++ src/main/java/l3/miage/shopcook/repositories/IngredientRepository.java
@@ -1,6 +1,5 @@
 package l3.miage.shopcook.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import l3.miage.shopcook.ingredients.Ingredient;
 
